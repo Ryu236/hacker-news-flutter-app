f57006f8e1457e8c2110a29b5bf7e7fc9830a906
Ryu236 <ryu.adv.2360@gmail.com>
2021-01-21 21:03:30 +0900

    :white_check_mark: test(driver): add a test of loading more items

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -4,6 +4,8 @@ import 'package:test/test.dart';
 void main() {
   group('Scroll listview', () {
     FlutterDriver driver;
+    final listFinder = find.byValueKey('hacker_news_list');
+    final itemFinder = find.byValueKey('load_more_button');
 
     setUpAll(() async {
       driver = await FlutterDriver.connect();
@@ -16,8 +18,23 @@ void main() {
     });
 
     test('verifies the list view and text button', () async {
-      final listFinder = find.byValueKey('hacker_news_list');
-      final itemFinder = find.byValueKey('load_more_button');
+      // wait for loading news items.
+      await driver.waitForAbsent(itemFinder);
+
+      await driver.scrollUntilVisible(
+        listFinder,
+        itemFinder,
+        dyScroll: -300.0,
+      );
+
+      expect(
+        await driver.getText(itemFinder),
+        'Load More',
+      );
+    });
+
+    test('tap text button and display more items', () async {
+      await driver.tap(itemFinder);
 
       // wait for loading news items.
       await driver.waitForAbsent(itemFinder);
